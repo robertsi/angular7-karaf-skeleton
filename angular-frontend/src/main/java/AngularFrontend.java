e45d08dfd8508f7bce83dd2970a975bb5bfd7b89
robertsi <robertsi@users.noreply.github.com>
2019-10-01 23:52:20 +0200

    Redirect 404 to index.html! angular routing

--- angular-frontend/src/main/java/AngularFrontend.java
+++ angular-frontend/src/main/java/AngularFrontend.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
+@Component(
+  service = AngularFrontend.class,
+ // scope= ServiceScope.PROTOTYPE,
+  property = {
+    HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PATTERN + " = /angular-frontend/*",
+    HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PREFIX + " = /META-INF"
+  }
+)
 public class AngularFrontend {
 }
--- angular-frontend/src/main/java/NotFoundError.java
+++ angular-frontend/src/main/java/NotFoundError.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class NotFoundError {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//https://angular.io/guide/deployment#routed-apps-must-fallback-to-indexhtml
+@Component(
+  service = Servlet.class,
+  //scope= ServiceScope.PROTOTYPE,
+  property = {
+    HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_ERROR_PAGE + " = 404",
+  }
+)
+public class NotFoundError extends HttpServlet {
+
+  Logger LOG = LoggerFactory.getLogger(NotFoundError.class);
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+    LOG.info("doGet executing: ");
+    try {
+      RequestDispatcher dispatcher =  req.getRequestDispatcher("angular-frontend/index.html");
+      dispatcher.forward(req, resp);
+//      resp.sendRedirect("angular-frontend/index.html");
+      LOG.info("doGet executed: ");
+    }
+    catch (Exception ex)
+    {
+      LOG.error("doGet error", ex);
+    }
+  }
 }
